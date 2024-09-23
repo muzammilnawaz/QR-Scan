9feb954db417c864f8c0f119bf86d68dcf4d9fec
muzammil khan <muzammilnawaz2935337@gmail.com>
2024-09-23 11:02:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const sizes = document.getElementById('sizes');
 const generatebtn = document.getElementById('generatebtn');
 const downloadbtn = document.getElementById('downloadbtn');
 
-const qrContainer = document.querySelector('qr-body');
+const qrContainer = document.querySelector('.qr-body');
 
 
 let size = sizes.value
 generatebtn.addEventListener('click', (e)=>{
     e.preventDefault();
+    console.log(qrText , sizes , generatebtn , downloadbtn , qrContainer);
     generateQRCode();
 });
 
 function generateQRCode(){
-    new generateQRCode(qrContainer,{
+    new QRCode(qrContainer,{
         text:qrText.value,
         height:size,
         width:size,
-        colorLight:"#fff"
-        colorDark:"#000"
+        colorLight:"#ffffff",
+        colorDark:"#000000",
     });
 }
