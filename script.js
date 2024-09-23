364a62e84c3396be247192df98b1f16998214bd7
muzammilnawaz <muzammilnawaz2935337@gmail.com>
2024-09-24 00:24:54 +0500

    scan

--- script.js
+++ script.js
@@ -9,11 +9,46 @@ const qrContainer = document.querySelector('.qr-body');
 let size = sizes.value
 generatebtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    console.log(qrText , sizes , generatebtn , downloadbtn , qrContainer);
-    generateQRCode();
+    isEmptyInput();
+
 });
 
+sizes.addEventListener('change',(e)=>{
+    size = e.target.value;
+    isEmptyInput();
+});
+
+downloadbtn.addEventListener('click' , ()=>{
+    let img = document.querySelector('.qr-body img ');
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadbtn.setAttribute("href" , imgAtrr);
+    }
+
+    else{
+        downloadbtn.setAttribute("href" , `${document.querySelector('canvas').toDataURL()}`);
+
+
+    }
+})
+
+function isEmptyInput(){
+    if(qrText.value.length > 0){
+        generateQRCode();
+    }
+
+    else{
+        alert("Enter the text or URL to Generate your QR Code")
+    }
+
+}
+
+
+
+
+
 function generateQRCode(){
+    qrContainer.innerHTML = ""
     new QRCode(qrContainer,{
         text:qrText.value,
         height:size,
